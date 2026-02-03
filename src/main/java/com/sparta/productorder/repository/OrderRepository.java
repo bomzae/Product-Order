02cf9d25acffaa3cb8a63e42e10474573068f8f1
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-02-03 15:26:50 +0900

    N+1 문제 방지

--- src/main/java/com/sparta/productorder/repository/OrderRepository.java
+++ src/main/java/com/sparta/productorder/repository/OrderRepository.java
@@ -1,8 +1,14 @@
 package com.sparta.productorder.repository;
 
 import com.sparta.productorder.entity.Orders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     Orders findByOrderId(long orderId);
+
+    @EntityGraph(attributePaths = {"product"})
+    Page<Orders> findAll(Pageable pageable);
 }
